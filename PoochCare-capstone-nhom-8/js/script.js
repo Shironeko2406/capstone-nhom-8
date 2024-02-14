1acfe4861c6f52f1ab3971804f5a310c4c32a57b
QuynhPhung <phungthithuquynh.91@gmail.com>
2024-02-14 12:29:06 +0700

    create basic cards

---
+++ PoochCare-capstone-nhom-8/js/script.js
@@ -0,0 +1,30 @@
+// <section class="w3l-clients" id="clients">
+//   <!-- /grids -->
+//   <div class="cusrtomer-layout py-5">
+//     <div class="container py-lg-4 py-md-3 pb-lg-0 HPTestimonials">
+//       <div class="heading text-center mx-auto">
+//         <h6 class="sub-title text-center">Testimonials</h6>
+//         <h3 class="hny-title mb-md-5 mb-4">
+//           Happy Clients &amp; Feedbacks
+//         </h3>
+//       </div>
+//       <div class="testimonial-width">
+//         <div
+//           id="owl-demo1"
+//           class="owl-two owl-carousel owl-theme owl-loaded owl-drag"
+//         >
+//           <div
+//             class="owl-stage-outer"
+//             style="
+//               transform: translate3d(0px, 0px, 0px);
+//               transition: all 0s ease 0s;
+//               width: 3210px;
+//             "
+//           >
+//             <div class="owl-item active"></div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </section>
